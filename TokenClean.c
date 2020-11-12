c485d93f88c0db3b19de44cd588a9cfbccf61eda
Diane <diane.leblanc.albarel@gmail.com>
2020-11-12 15:50:26 +0100

    fixed seg fault problem on the size of table + adding signatre system

--- TokenClean.c
+++ TokenClean.c
@@ -1,3 +1,4 @@
+
 #include <string.h>
 #include <stdint.h>
 #include <time.h>
@@ -24,7 +25,7 @@
 #define MAXUSES 3
 
 #define ROW_BYTES 32
-
+#define NUM_ROWS  1000000
 #define RANDBYTES 4
 
 #define CARD_T uint64_t
@@ -32,9 +33,11 @@
 #define USES_T uint8_t
 #define TIME_T uint64_t
 #define TOKEN_T uint32_t
+#define SIGN_T uint64_t
+#define PK_T uint64_t
 
 static const uint8_t zero_row[ROW_BYTES] = { 0 };
-int NUM_ROWS =90000000;
+
 
 // CODE TAKEN FROM https://wiki.openssl.org/index.php/EVP_Symmetric_Encryption_and_Decryption
 void handleErrors(void){
@@ -84,74 +87,78 @@ int decrypt(unsigned char *ciphertext, int ciphertext_len, unsigned char *key,
 
 
 TIME_T get_time(struct timeval t){
-    double sum;
-    uint64_t seconds = t.tv_sec;
+        double sum;
+        uint64_t seconds = t.tv_sec;
     uint64_t microseconds = t.tv_usec;
-    sum = seconds + microseconds*1e-6; //1e-6 over integers ? mayber float or double instead
-    return sum;
+        sum = seconds + microseconds*1e-6; //1e-6 over integers ? mayber float or double instead
+        return sum;
 }
 
 double get_time_execution(struct timeval begin, struct timeval end){
-    double sum;
-    long seconds = end.tv_sec - begin.tv_sec;
+        double sum;
+        long seconds = end.tv_sec - begin.tv_sec;
     long microseconds = end.tv_usec - begin.tv_usec;
-    sum = seconds + microseconds*1e-6;
-    return sum;
+        sum = seconds + microseconds*1e-6;
+        return sum;
 }
 
-int tokenisation(uint8_t *table, CARD_T cb, USES_T uses, TIME_T deadline, TOKEN_T *tokenToReturn, unsigned char key[32], unsigned char iv[16]){
+int tokenisation(uint8_t *table, CARD_T cb, USES_T uses, TIME_T deadline, PK_T pk, TOKEN_T *tokenToReturn, unsigned char key[32], unsigned char iv[16]){
         uint8_t buffer[4];
-        uint8_t hash[HASH_LENGTH]; // verif size
+        uint8_t hash[HASH_LENGTH];
         TOKEN_T token;
 
         int timepast = 0;
         struct timeval begin, end;
         double delta;
 
+    uint32_t random = -1;
+
         uint8_t row[ROW_BYTES] = { 0 };
         memcpy(row, &cb, 8);
         memcpy(row+12, &uses, 1);
         memcpy(row+13, &deadline, 8);
+        memcpy(row+21, &pk,8);
 
         gettimeofday(&begin,0);
 
         do{
+                do{
             RAND_bytes(buffer, RANDBYTES);
                 memcpy(row+8, buffer, 4);
+            SHA224((const unsigned char *)row, ROW_BYTES, hash);
+                memcpy(&random, hash,4);
+                }while(random > 0b11111010010101101110101000000000  );
 
-            SHA224((const unsigned char *)row, ROW_BYTES, hash); //verifier la taille de hash?
-            token =((* (uint32_t *)hash)) % NUM_ROWS;
-
+            token = random % NUM_ROWS;
             gettimeofday(&end,0);
             delta = get_time_execution(begin,end);
         }while ( memcmp (zero_row, table + token*ROW_BYTES, 8) && delta<TIMEFRAME);
 
         if ( !memcmp(zero_row, table+token*ROW_BYTES, 8)){
-//          memcpy(table+token*ROW_BYTES, &row, ROW_BYTES);
 
                 int ctlen;
         ctlen = encrypt (row, 31, key, iv, table+token*ROW_BYTES);
-//              printf("ctlen = %d ",ctlen);
 
             *tokenToReturn = token;
-//      printf("Token %d created successfully \n",token);
                 return 1;// one for success
         }
         else{
-//      printf("Token creation failed \n");
             return 0;// zero for failure
         }
-
-        free(hash);
 }
 
-void clean(uint8_t * row, unsigned char key[32], unsigned char iv[16]){
+int cardIsValid(CARD_T card){
+        return (int) card;
+}
+
+void clean(uint8_t * row, uint8_t * table, unsigned char key[32], unsigned char iv[16]){
     struct timeval tdays;
     gettimeofday(&tdays,0);
     TIME_T now = get_time(tdays);
         TIME_T expiry;
 
-//      printf("cleaning\n");
+        uint8_t hash[HASH_LENGTH];
+        TOKEN_T token;
 
         if(memcmp(zero_row, row, 32)){
 
@@ -159,18 +166,25 @@ void clean(uint8_t * row, unsigned char key[32], unsigned char iv[16]){
             decrypt(row, 32, key, iv, drow);
                 memcpy(&expiry, drow+13, 8);
 
-           if ( !memcmp(zero_row, drow+12,1)  || expiry < now){ // Add CB cheksum and hash for address
-               memset(row, 0, ROW_BYTES);
-           }
+            SHA224((const unsigned char *)row, ROW_BYTES, hash);
+            token =((* (uint32_t *)hash)) % NUM_ROWS;
+
+                if ( !memcmp(zero_row, drow+12,1)  || expiry < now || cardIsValid((CARD_T) drow[0]) || (row == table+token*ROW_BYTES) ){
+                        memset(row, 0, ROW_BYTES);
+                }
         }
 }
 
 void cleanTable(uint8_t *table, unsigned char key[32], unsigned char iv[16] ){
-    for (int i=0;i<NUM_ROWS;i++)
-        clean(table+i*ROW_BYTES, key, iv);
+    for (uint64_t i=0; i<NUM_ROWS; i++)
+        clean(table+i*ROW_BYTES, table, key, iv);
 }
 
-void detokenisation(uint8_t *table, TOKEN_T token, CARD_T *card, unsigned char key[32], unsigned char iv[16] ){
+int signatureIsValid(PK_T * pk, SIGN_T * signature){
+        return !memcmp(pk,signature,8);
+}
+
+void detokenisation(uint8_t *table, TOKEN_T token, CARD_T * card, SIGN_T * signature, unsigned char key[32], unsigned char iv[16] ){
     struct timeval tempTime;
     gettimeofday(&tempTime,0);
     TIME_T now = get_time(tempTime);
@@ -181,75 +195,65 @@ void detokenisation(uint8_t *table, TOKEN_T token, CARD_T *card, unsigned char k
     decrypt(row, 32, key, iv, drow);
 
         memcpy(&expiry, drow+13, 8);
-    if( !memcmp(zero_row, drow+12,1) && expiry > now ){ //token is valid
+    if( memcmp(zero_row, drow+12,1) && expiry > now && signatureIsValid((PK_T *) drow+21, signature) ) { //token is valid
         memcpy(card, drow, 8);
 
                 if ( row[12] > 1 )
              row[12] --;
         else{
             printf("Maximal number of uses reached, cleaning token\n");
-            clean(row, key, iv);
+            clean(row, table, key, iv);
         }
     }
     else{
         printf("Token is invalid or obsolete\n");
-        clean(row, key, iv);
+        clean(row, table, key, iv);
         exit(1);
     }
 
 }
-void print_usage(char *argv[]) {
-	printf("usage: %s [-r <val>] \n", argv[0]);
-	exit(EXIT_FAILURE);
 
-}
+int main(){
+        printf("Token Vault Started\n");
 
-int main(int argc, char *argv[]){
-    char opt;
-    //NUM_ROWS=0;
-    printf("NUM_ROW = %d\n",NUM_ROWS);
-        struct timeval t;
+        struct timeval begin,end,t;
         time_t tt;
-        clock_t firstToken,temp, lastToken, sumToken=0, tempTime, cleanTime;
+        clock_t firstToken=0, lastToken=0, sumToken=0, tempTime, cleanTime,temp;
         double first, last, mean;
 
         CARD_T cb;
+        SIGN_T pk;
 
         unsigned char key[32];
         unsigned char iv[16];
 
         RAND_bytes(key, 32);
         RAND_bytes(iv,16);
-        
-        uint8_t * table= malloc(NUM_ROWS * ROW_BYTES*sizeof(uint8_t));
-        
+
+        uint8_t * table=calloc(NUM_ROWS * ROW_BYTES,sizeof(uint8_t));
         TOKEN_T token;
         int try;
 
+        gettimeofday(&begin,0);
         gettimeofday(&t,0);
         TIME_T expiry = get_time(t) + LIFESPAN;
 
-        for (int i = 0; i<NUM_ROWS; i++){ // en fonction de l'expé
+        for (int i = 0; i<NUM_ROWS; i++){
             cb = i;
+            pk = i;
             if(i==0){
                 temp=clock();
                 tempTime = clock();
                 
-                try = tokenisation(table, cb, MAXUSES, expiry, &token, key, iv);
+                try = tokenisation(table, cb, MAXUSES, expiry,pk, &token, key, iv);
                 
                 sumToken += clock() - tempTime;
                 firstToken = clock()-temp;
             }
-            else if (i <NUM_ROWS-1 ){
-                tempTime = clock();
-                try = tokenisation(table, cb, MAXUSES, expiry, &token, key, iv);
-                sumToken += clock() - tempTime;
-            }
-            else{
-                printf("LAST\n");
+            else {
                 temp=clock();
                 tempTime = clock();
-                try = tokenisation(table, cb, MAXUSES, expiry, &token, key, iv);
+                try = tokenisation(table, cb, MAXUSES, expiry,pk, &token, key, iv);
                 sumToken += clock() - tempTime;
                 lastToken=clock()-temp;
             }
@@ -258,18 +262,18 @@ int main(int argc, char *argv[]){
                 break;
             }
         }
-
+        gettimeofday(&end,0);
         printf("TOKENS CREATED\n");
 
-        uint64_t numberInsert = 0;
-        for(int i=0; i<NUM_ROWS; i++){
+        uint64_t numberInsert = 1;
+        for(uint64_t i=0; i<NUM_ROWS; i++){
             if(memcmp(zero_row,table+i*ROW_BYTES,8)) numberInsert++; //if not zero, increment
         }
 
         printf("TOKENS counted\n");
 
 
-        mean = ((double)sumToken/CLOCKS_PER_SEC)/NUM_ROWS;
+        mean = ((double)sumToken/CLOCKS_PER_SEC)/numberInsert;
         first = (double)firstToken/CLOCKS_PER_SEC;
         last = (double)lastToken/CLOCKS_PER_SEC;
 
@@ -290,12 +294,10 @@ int main(int argc, char *argv[]){
                 exit(1);
             }
                 else{
-                        fprintf(fp,"total: %f, mean : %f, first : %f, last %f, number insert : %lld size : %d clean time : %f\n", (double) sumToken/CLOCKS_PER_SEC, mean, first, last,(long long int) numberInsert,NUM_ROWS, (double)cleanTime/CLOCKS_PER_SEC);
+                        fprintf(fp,"total: %f, mean : %f, first : %f, last %f, number insert : %lld size : %d clean time : %f\n", get_time_execution(begin,end), mean, first, last,(long long int) numberInsert,NUM_ROWS, (double)cleanTime/CLOCKS_PER_SEC);
                         fclose(fp);
                 }
 
-        printf("total: %f, mean : %f, first : %f, last %f\n", (double) sumToken/CLOCKS_PER_SEC, mean, first, last);
-    free(table);
+        printf("total: %f, mean : %f, first : %f, last %f\n", get_time_execution(begin,end), mean, first, last);
 
 }
-
