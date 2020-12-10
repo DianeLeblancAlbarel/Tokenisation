7c821d6420f0b366ed55078ea415f0750890d3ae
cnugier <74309885+cnugier@users.noreply.github.com>
2020-12-10 17:18:41 +0100

    Update token.c

--- token.c
+++ token.c
@@ -1,52 +1,5 @@
-#include <string.h>
-#include <stdint.h>
-#include <time.h>
-#include <signal.h>
-#include <sys/time.h>
-#include <unistd.h>
-#include <math.h>
-#include <pthread.h>
-#include <time.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <openssl/sha.h>
-#include <openssl/rand.h>
-#include <openssl/aes.h>
-#include <openssl/conf.h>
-#include <openssl/evp.h>
-#include <openssl/err.h>
 #include "tools.h"
 #include "token.h"
-
-#define HASH_LENGTH 2*SHA224_DIGEST_LENGTH+1
-
-#define TIMEFRAME 0.1
-#define LIFESPAN 10000
-#define MAXUSES 3
-#define ROW_BYTES 32
-#define NUM_ROWS  10000
-#define RANDBYTES 4
-
-#define CARD_T uint64_t
-#define RAND_T uint32_t
-#define USES_T uint8_t
-#define TIME_T uint64_t
-#define TOKEN_T uint32_t
-#define SIGN_T uint64_t
-#define PK_T uint64_t
-
-#define RED   "\x1B[31m"
-#define GRN   "\x1B[32m"
-#define YEL   "\x1B[33m"
-#define BLU   "\x1B[34m"
-#define MAG   "\x1B[35m"
-#define CYN   "\x1B[36m"
-#define WHT   "\x1B[37m"
-#define RESET "\x1B[0m"
-
-static const uint8_t zero_row[ROW_BYTES] = { 0 };
-
-// Upycling Token Table
 
 int tokenization(uint8_t *table, CARD_T cb, USES_T uses, TIME_T deadline, PK_T  pk, TOKEN_T *tokenToReturn, unsigned char key[32], unsigned char iv[16]){
     uint8_t buffer[4];
@@ -81,9 +34,9 @@ int tokenization(uint8_t *table, CARD_T cb, USES_T uses, TIME_T deadline, PK_T  
     if ( !memcmp(zero_row, table+token*ROW_BYTES, 8)){
         encrypt (row, 31, key, iv, table+token*ROW_BYTES);
         *tokenToReturn = token;
-        return 1;  
+        return 1;
     }
-    else return 0; 
+    else return 0;
 }
 
 void detokenization(uint8_t *table, TOKEN_T token, CARD_T * card, SIGN_T signature, unsigned char key[32], unsigned char iv[16] ){
@@ -95,7 +48,7 @@ void detokenization(uint8_t *table, TOKEN_T token, CARD_T * card, SIGN_T signatu
 
     uint8_t drow[32];
     decrypt(row, 32, key, iv, drow);
-    
+
     memcpy(&expiry, drow+13, 8);
     if( memcmp(zero_row, drow+12,1) && expiry > now && signatureIsValid((PK_T *) drow+21, &signature) ) { //token is valid
         memcpy(card, drow, 8);
